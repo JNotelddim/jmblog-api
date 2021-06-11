174c7347e75cf28f6b3ce0b64ab9f9834eb6333d
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-06-11 15:18:09 -0700

    Establish alternate route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
+// Decorators
 import { Injectable } from '@nestjs/common';
 
+// Models
+
+// Exports
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUsers(): string {
+    return 'test get users';
   }
 }
