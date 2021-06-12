1df3b59a874d77d341cc9d30c90314d541f8d5b7
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-06-12 10:15:50 -0700

    Establish template for user/:id endpoint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from 'src/models';
 import { UserService } from '../services/user.service';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/user')
+  @Get('/users')
   async getUsers(): Promise<string> {
     return await this.userService.getUsers();
+  }
+
+  @Get('/user/:id')
+  async getUser(@Param('id') id: string): Promise<Partial<User>> {
+    return await this.userService.getUser(id);
   }
 }
