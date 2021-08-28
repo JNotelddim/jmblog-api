6a5152f5ece3bbd1503a984581b98591999cd135
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-08-28 12:29:31 -0700

    Add endpoint for getting a full post object by id
    and update /posts to not return the full post objects, but a summarized
    version

--- src/interfaces/post.interface.ts
+++ src/interfaces/post.interface.ts
@@ -2,8 +2,15 @@ import { Document } from 'mongoose';
 
 export interface Post extends Document {
   readonly authorId: string;
-  readonly contents: string;
+  readonly content: string;
   readonly createdAt: Date;
   readonly modifiedAt: Date;
   readonly title: string;
+}
+
+export interface SummarizedPost {
+  authorId: string;
+  title: string;
+  createdAt: Date;
+  summary: string;
 }
