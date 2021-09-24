36b90cffef8d82adb60ffbe20b179cff28794391
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-09-24 13:04:02 -0700

    Add 'summary' to official post schema

--- src/posts/posts.dtos.ts
+++ src/posts/posts.dtos.ts
@@ -1,11 +1,13 @@
 export interface CreatePostDto {
   title: string;
   content: string;
+  summary: string;
   author: string;
 }
 
 export interface UpdatePostDto {
   title: string;
   content: string;
+  summary: string;
   id: string;
 }
