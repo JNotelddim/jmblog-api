94ba06b707c069f5882f9cdeee7214873b5d30ea
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-10-14 14:11:53 -0700

    Add procfile and alternate running port

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.RUNNING_PORT, () => {
+  await app.listen(process.env.RUNNING_PORT || 8080, () => {
     console.log(`Blog Api listening on port ${process.env.RUNNING_PORT}`);
   });
 }
