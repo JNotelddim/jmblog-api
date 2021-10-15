2f2229a9d10f392a00f462eb510acb82b9608127
Jared Middleton <jaredmiddleton3.14@gmail.com>
2021-10-15 11:00:35 -0700

    Update naming of PORT env var
    Heroku auto-assigns a port and it comes in through the `PORT` env var,
    so my custom variable name won't mesh with that

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.RUNNING_PORT || 8080, () => {
-    console.log(`Blog Api listening on port ${process.env.RUNNING_PORT}`);
+  await app.listen(process.env.PORT || 8080, () => {
+    console.log(`Blog Api listening on port ${process.env.PORT || 8080}`);
   });
 }
 
